1ee48af005be8131ec27bfd7e240839c147ef3a3
rizkipurnomo <rizkidwijo@gmail.com>
2020-09-02 12:57:33 +0700

    add aplication router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Register from './Register';
+import {
+  BrowserRouter as Router, Switch, Route
+} from "react-router-dom";
+import Home from './Home';
+
+/*import { Button } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
+import { Segment } from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
+*/
+//import { Button, Header, Segment, Form,  Label } from 'semantic-ui-react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+     <Switch>
+       <Route exact path='/'>
+         <Home/>
+       </Route>
+       <Route path='/register'>
+         <Register/>
+       </Route>
+     </Switch>
+   </Router>  
+    
+    );
 }
 
 export default App;
